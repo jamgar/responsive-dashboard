7393bd9b3a7807833e3e410fa8bedc6862c01ad2
github username <github email address>
2018-03-30 10:33:23 -0500

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,6 @@
+require('./css/style.css')
+const init = () => {
+  document.getElementById('root').innerHTML = "Working..."
+}
+
+document.addEventListener('DOMContentLoaded', init)
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+}
